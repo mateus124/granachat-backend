63a5d670595d65ba9241b5b92294acc7bbe18cf9
mateus124 <alvesmateusq00@gmail.com>
2026-01-27 22:31:25 -0300

    fix: tags

--- app/main.py
+++ app/main.py
@@ -7,8 +7,20 @@ from api.summary import router as summary_router
 
 Base.metadata.create_all(bind=engine)
 
+tags_metadata = [
+    {
+        "name": "Chat",
+        "description": "Operações de envio de mensagens",
+    },
+    {
+        "name": "ProdSummaryuto",
+        "description": "Operações de relatórios",
+    },
+]
+
 app = FastAPI(
     title="Granachat API",
+    openapi_tags=tags_metadata,
     description="API de controle financeiro via chat",
     version="0.1.0"
 )
